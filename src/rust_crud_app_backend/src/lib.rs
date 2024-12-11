109cb2d3794d097cfb0e43175c34bc9d827ca9f9
Prabhat <45896902+artech-git@users.noreply.github.com>
2024-12-11 10:23:16 +0530

    feat(pagination)!: add pagination to the list of users for stable memory

--- src/rust_crud_app_backend/src/lib.rs
+++ src/rust_crud_app_backend/src/lib.rs
@@ -1,25 +1,31 @@
-use candid::Nat;
 use ic_cdk::{println, query, update};
+use ic_stable_structures::memory_manager::{MemoryId, MemoryManager, VirtualMemory};
 use todo_type::{TodoText, TodoUnit, UID};
 // use std::borrow::Borrow;
 use ic_stable_structures::btreemap::BTreeMap;
-use ic_stable_structures::DefaultMemoryImpl;
+use ic_stable_structures::{DefaultMemoryImpl, MinHeap};
 use std::cell::RefCell;
 
 pub mod app;
 pub mod todo_type;
 pub mod utils;
 
-
-//TODO: work on ensuring memory metrics doesn't get overfilled   
-
+//TODO: work on ensuring memory metrics doesn't get overfilled
+type Memory = VirtualMemory<DefaultMemoryImpl>;
 // declare the storage region for todo in canister
 thread_local! {
-    static TODO_DATA: RefCell<BTreeMap<UID, TodoUnit, DefaultMemoryImpl>> =
-        RefCell::new(BTreeMap::init(DefaultMemoryImpl::default()));
 
-    static TODO_DATA_PAGE: RefCell<Nat> = RefCell::new(Nat::from(0_u32));
-    // static TODO_PAGINATION: RefCell<Iter<'_, UID, TodoUnit, DefaultMemoryImpl>> = RefCell::new(TODO_DATA.with(|todo_data| todo_data.borrow().iter()));
+    static MEMORY_MANAGER: RefCell<MemoryManager<DefaultMemoryImpl>> =
+        RefCell::new(MemoryManager::init(DefaultMemoryImpl::default()));
+
+    static TODO_DATA: RefCell<BTreeMap<UID, TodoUnit, Memory>> =
+        RefCell::new(BTreeMap::init(
+            MEMORY_MANAGER.with(|m| m.borrow().get(MemoryId::new(0)))
+        ));
+
+    static TODO_DATA_PAGE: RefCell<MinHeap<(u64, UID), Memory>> = RefCell::new(MinHeap::init(
+        MEMORY_MANAGER.with(|m| m.borrow().get(MemoryId::new(1)))
+    ).unwrap());
 }
 
 /// Router for submitting a todo information.
@@ -27,14 +33,21 @@ thread_local! {
 fn generate_new_todo(todo_text: TodoText) -> UID {
     //TODO: apply data size assertion checks: Result should be used somehow
 
-    TODO_DATA.with(|todo_data| {
-        // generate a uid of 32 bit sized
-        let uid_string = utils::generate_random_string(32);
+    // generate a uid of 32 bit sized
+    let uid_string = utils::generate_random_string(32);
+    let curr_time = ic_cdk::api::time();
 
+    TODO_DATA_PAGE.with(|todo_data_page| {
+        let _val = todo_data_page
+            .borrow_mut()
+            .push(&(curr_time, uid_string.clone()));
+    });
+
+    TODO_DATA.with(|todo_data| {
         let todo_unit = TodoUnit {
             uid: uid_string.clone(),
             todo_text: todo_text,
-            start_time: ic_cdk::api::time(),
+            start_time: curr_time,
         };
 
         let _val = todo_data.borrow_mut().insert(uid_string.clone(), todo_unit);
@@ -64,21 +77,17 @@ fn read_pagination(page_size: usize, count: usize) -> Vec<TodoUnit> {
     let start = page_size.saturating_mul(count.saturating_sub(1));
     let end = page_size.saturating_mul(count);
 
-    TODO_DATA.with(|todo_data| {
+    TODO_DATA_PAGE.with(|todo_data| {
         let mut todo_vec = vec![];
-        for (idx, todo_unit) in todo_data
-            .borrow()
-            .values() // assuming the iterator behaviour to be deterministic each time we invoke this function,
-            .enumerate()
+
+        for (idx, todo_id) in todo_data.borrow().iter().skip(start).take(end)
+        // .values() // assuming the iterator behaviour to be deterministic each time we invoke this function,
         {
-            // also making a strong assumption regarinding insertion of values towards end of the map
-            if idx < start {
-                continue;
-            }
-            if idx >= end {
-                break;
-            }
-            todo_vec.push(todo_unit.clone());
+            TODO_DATA.with(|todo_data| {
+                if let Some(todo_unit) = todo_data.borrow().get(&todo_id) {
+                    todo_vec.push(todo_unit.clone());
+                }
+            });
         }
         todo_vec
     })
